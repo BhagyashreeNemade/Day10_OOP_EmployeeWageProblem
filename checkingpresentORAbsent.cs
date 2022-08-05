bd1e195eb67522d10e90cf52a4acf56bbef9db5b
BhagyashreeNemade <108821937+BhagyashreeNemade@users.noreply.github.com>
2022-08-05 16:52:44 +0530

    checking present OR Absent

---
+++ checkingpresentORAbsent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8_OOP_EmployeeWageProblem
+{
+    public class checkingpresentORAbsent
+    {
+        bool present;
+        public void CheckingpresentORAbsent()
+
+        {
+            Random value = new Random();
+
+            int empCheck = value.Next(0, 2);
+
+
+            if (empCheck == 1)
+            {
+                present = true;
+
+                Console.WriteLine(" full timeEmployee is present");
+
+
+            }
+            else
+            {
+                present = false;
+                Console.WriteLine(" full timeEmployee is Absent");
+            }
+        }
+
+    }
+
+}
